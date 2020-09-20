d9a36431caddeff39276839eb31ac643b01b5354
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-09-20 16:52:55 +0100

    update .clang-format file and run

--- include/as-camera/as-camera-controller.hpp
+++ include/as-camera/as-camera-controller.hpp
@@ -8,15 +8,15 @@ namespace asc
 
 enum class MotionType
 {
-    // clang-format off
-    None     = 0,
-    Forward  = 1 << 0,
-    Backward = 1 << 1,
-    Left     = 1 << 2,
-    Right    = 1 << 3,
-    Up       = 1 << 4,
-    Down     = 1 << 5
-    // clang-format on
+  // clang-format off
+  None     = 0,
+  Forward  = 1 << 0,
+  Backward = 1 << 1,
+  Left     = 1 << 2,
+  Right    = 1 << 3,
+  Up       = 1 << 4,
+  Down     = 1 << 5
+  // clang-format on
 };
 
 } // namespace asc
@@ -24,7 +24,7 @@ enum class MotionType
 template<>
 struct bec::EnableBitMaskOperators<asc::MotionType>
 {
-    static const bool Enable = true;
+  static const bool Enable = true;
 };
 
 namespace asc
@@ -32,78 +32,78 @@ namespace asc
 
 enum class Handedness
 {
-    Left,
-    Right
+  Left,
+  Right
 };
 
 struct CameraControl
 {
-    float pitch;
-    float yaw;
-    float dolly;
-    MotionType motion;
+  float pitch;
+  float yaw;
+  float dolly;
+  MotionType motion;
 };
 
 struct CameraProperties
 {
-    float translate_speed;
-    float rotate_speed;
-    float look_smoothness;
+  float translate_speed;
+  float rotate_speed;
+  float look_smoothness;
 };
 
 inline void updateCamera(
-    Camera& camera, const CameraControl& control, const CameraProperties& props,
-    const float dt, const Handedness handedness)
+  Camera& camera, const CameraControl& control, const CameraProperties& props,
+  const float dt, const Handedness handedness)
 {
-    const as::mat3_t orientation = camera.transform().rotation;
+  const as::mat3_t orientation = camera.transform().rotation;
 
-    using bec::operator&;
+  using bec::operator&;
 
-    const auto movement = [props, dt](const Handedness handedness) {
-        switch (handedness) {
-            case Handedness::Left:
-                return props.translate_speed * dt;
-            case Handedness::Right:
-                return -props.translate_speed * dt;
-            default:
-                return 0.0f;
-        }
-    };
-
-    if ((control.motion & MotionType::Forward) == MotionType::Forward) {
-        camera.look_at += as::mat3_basis_z(orientation) * movement(handedness);
+  const auto movement = [props, dt](const Handedness handedness) {
+    switch (handedness) {
+      case Handedness::Left:
+        return props.translate_speed * dt;
+      case Handedness::Right:
+        return -props.translate_speed * dt;
+      default:
+        return 0.0f;
     }
+  };
 
-    if ((control.motion & MotionType::Backward) == MotionType::Backward) {
-        camera.look_at -= as::mat3_basis_z(orientation) * movement(handedness);
-    }
+  if ((control.motion & MotionType::Forward) == MotionType::Forward) {
+    camera.look_at += as::mat3_basis_z(orientation) * movement(handedness);
+  }
 
-    if ((control.motion & MotionType::Left) == MotionType::Left) {
-        camera.look_at -=
-            as::mat3_basis_x(orientation) * props.translate_speed * dt;
-    }
+  if ((control.motion & MotionType::Backward) == MotionType::Backward) {
+    camera.look_at -= as::mat3_basis_z(orientation) * movement(handedness);
+  }
 
-    if ((control.motion & MotionType::Right) == MotionType::Right) {
-        camera.look_at +=
-            as::mat3_basis_x(orientation) * props.translate_speed * dt;
-    }
+  if ((control.motion & MotionType::Left) == MotionType::Left) {
+    camera.look_at -=
+      as::mat3_basis_x(orientation) * props.translate_speed * dt;
+  }
 
-    if ((control.motion & MotionType::Up) == MotionType::Up) {
-        camera.look_at +=
-            as::mat3_basis_y(orientation) * props.translate_speed * dt;
-    }
+  if ((control.motion & MotionType::Right) == MotionType::Right) {
+    camera.look_at +=
+      as::mat3_basis_x(orientation) * props.translate_speed * dt;
+  }
 
-    if ((control.motion & MotionType::Down) == MotionType::Down) {
-        camera.look_at -=
-            as::mat3_basis_y(orientation) * props.translate_speed * dt;
-    }
+  if ((control.motion & MotionType::Up) == MotionType::Up) {
+    camera.look_at +=
+      as::mat3_basis_y(orientation) * props.translate_speed * dt;
+  }
 
-    // https://www.gamasutra.com/blogs/ScottLembcke/20180404/316046/Improved_Lerp_Smoothing.php
-    const float rate = exp2(props.look_smoothness);
-    const float t = exp2(-rate * dt);
-    camera.pitch = as::lerp(t, control.pitch, camera.pitch);
-    camera.yaw = as::lerp(t, control.yaw, camera.yaw);
-    camera.focal_dist = as::lerp(t, control.dolly, camera.focal_dist);
+  if ((control.motion & MotionType::Down) == MotionType::Down) {
+    camera.look_at -=
+      as::mat3_basis_y(orientation) * props.translate_speed * dt;
+  }
+
+  // https://www.gamasutra.com/blogs/ScottLembcke/20180404/316046/Improved_Lerp_Smoothing.php
+  const float rate = exp2(props.look_smoothness);
+  const float t = exp2(-rate * dt);
+  camera.pitch = as::lerp(t, control.pitch, camera.pitch);
+  camera.yaw = as::lerp(t, control.yaw, camera.yaw);
+  camera.focal_dist = as::lerp(t, control.dolly, camera.focal_dist);
 }
 
 } // namespace asc
--- include/as-camera/as-camera.hpp
+++ include/as-camera/as-camera.hpp
@@ -7,29 +7,29 @@ namespace asc
 
 struct Camera
 {
-    as::point3_t look_at{0.0f}; // position of camera when zero focal_dist
-                                // position of look_at when non-zero focal_dist
-    float yaw{0.0f};
-    float pitch{0.0f};
-    float focal_dist{0.0f}; // zero gives fps free look,
-                            // otherwise orbit about look_at
+  as::point3_t look_at{0.0f}; // position of camera when zero focal_dist
+                              // position of look_at when non-zero focal_dist
+  float yaw{0.0f};
+  float pitch{0.0f};
+  float focal_dist{0.0f}; // zero gives fps free look,
+                          // otherwise orbit about look_at
 
-    as::affine_t view() const;      // camera view to pass to shader
-    as::affine_t transform() const; // camera transform to use in app
+  as::affine_t view() const; // camera view to pass to shader
+  as::affine_t transform() const; // camera transform to use in app
 };
 
 inline as::affine_t Camera::view() const
 {
-    return as::affine_inverse(transform());
+  return as::affine_inverse(transform());
 }
 
 inline as::affine_t Camera::transform() const
 {
-    return as::affine_mul(
-        as::affine_mul(
-            as::affine_t(as::vec3_t::axis_z(focal_dist)),
-            as::affine_t(as::mat3_rotation_zxy(pitch, yaw, 0.0f))),
-        as::affine_from_point3(look_at));
+  return as::affine_mul(
+    as::affine_mul(
+      as::affine_t(as::vec3_t::axis_z(focal_dist)),
+      as::affine_t(as::mat3_rotation_zxy(pitch, yaw, 0.0f))),
+    as::affine_from_point3(look_at));
 }
 
 } // namespace asc
