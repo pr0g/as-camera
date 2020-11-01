d970546cee3437f8d2b0a2081f9c9ef672fbb1c6
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2020-11-01 18:04:23 +0000

    first pass of camera updates

--- include/as-camera/as-camera-controller.hpp
+++ include/as-camera/as-camera-controller.hpp
@@ -9,13 +9,16 @@ namespace asc
 enum class MotionType
 {
   // clang-format off
-  None     = 0,
-  Forward  = 1 << 0,
-  Backward = 1 << 1,
-  Left     = 1 << 2,
-  Right    = 1 << 3,
-  Up       = 1 << 4,
-  Down     = 1 << 5
+  None        = 0,
+  Forward     = 1 << 0,
+  Backward    = 1 << 1,
+  Left        = 1 << 2,
+  Right       = 1 << 3,
+  Up          = 1 << 4,
+  Down        = 1 << 5,
+  ScrollIn    = 1 << 6,
+  ScrollOut   = 1 << 7,
+  ScrollReset = 1 << 8,
   // clang-format on
 };
 
@@ -38,6 +41,7 @@ enum class Handedness
 
 struct CameraControl
 {
+  as::vec3 look_at;
   float pitch;
   float yaw;
   float dolly;
@@ -52,7 +56,7 @@ struct CameraProperties
 };
 
 inline void updateCamera(
-  Camera& camera, const CameraControl& control, const CameraProperties& props,
+  Camera& camera, CameraControl& control, const CameraProperties& props,
   const float dt, const Handedness handedness)
 {
   const as::mat3 orientation = camera.transform().rotation;
@@ -70,32 +74,53 @@ inline void updateCamera(
     }
   };
 
+  const auto flatten = [&camera](const auto& vector) {
+    return std::abs(camera.focal_dist) > 0.0f
+        ? as::vec_normalize(as::vec3(vector.x, 0.0f, vector.z))
+        : vector;
+  };
+
+  const auto basis_z = as::mat3_basis_z(orientation);
+  const auto basis_x = as::mat3_basis_x(orientation);
+
+  const auto forward = flatten(basis_z);
+  const auto right = flatten(basis_x);
+
   if ((control.motion & MotionType::Forward) == MotionType::Forward) {
-    camera.look_at += as::mat3_basis_z(orientation) * movement(handedness);
+    control.look_at += forward * movement(handedness);
   }
 
   if ((control.motion & MotionType::Backward) == MotionType::Backward) {
-    camera.look_at -= as::mat3_basis_z(orientation) * movement(handedness);
+    control.look_at -= forward * movement(handedness);
   }
 
   if ((control.motion & MotionType::Left) == MotionType::Left) {
-    camera.look_at -=
-      as::mat3_basis_x(orientation) * props.translate_speed * dt;
+    control.look_at -= right * props.translate_speed * dt;
   }
 
   if ((control.motion & MotionType::Right) == MotionType::Right) {
-    camera.look_at +=
-      as::mat3_basis_x(orientation) * props.translate_speed * dt;
+    control.look_at += right * props.translate_speed * dt;
   }
 
   if ((control.motion & MotionType::Up) == MotionType::Up) {
-    camera.look_at +=
-      as::mat3_basis_y(orientation) * props.translate_speed * dt;
+    control.look_at += as::vec3::axis_y() * props.translate_speed * dt;
   }
 
   if ((control.motion & MotionType::Down) == MotionType::Down) {
-    camera.look_at -=
-      as::mat3_basis_y(orientation) * props.translate_speed * dt;
+    control.look_at -= as::vec3::axis_y() * props.translate_speed * dt;
+  }
+
+  if ((control.motion & MotionType::ScrollIn) == MotionType::ScrollIn) {
+    control.dolly = as::min(control.dolly + props.translate_speed * dt, 0.0f);
+  }
+
+  if ((control.motion & MotionType::ScrollOut) == MotionType::ScrollOut) {
+    control.dolly -= props.translate_speed * dt;
+  }
+
+  if ((control.motion & MotionType::ScrollReset) == MotionType::ScrollReset) {
+    control.look_at = camera.transform().translation;
+    control.dolly = 0.0f;
   }
 
   // https://www.gamasutra.com/blogs/ScottLembcke/20180404/316046/Improved_Lerp_Smoothing.php
@@ -104,6 +129,7 @@ inline void updateCamera(
   camera.pitch = as::mix(control.pitch, camera.pitch, t);
   camera.yaw = as::mix(control.yaw, camera.yaw, t);
   camera.focal_dist = as::mix(control.dolly, camera.focal_dist, t);
+  camera.look_at = as::vec_mix(control.look_at, camera.look_at, t);
 }
 
 } // namespace asc
