182cefcfe75968fcf2fdccd3ce412db1b2b94ef1
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-01-17 15:23:36 +0000

    update as-camera.hpp to use as::real as opposed to float

--- include/as-camera/as-camera.hpp
+++ include/as-camera/as-camera.hpp
@@ -5,15 +5,17 @@
 namespace asc
 {
 
+using as::operator""_r;
+
 struct Camera
 {
-  as::vec3 look_at{0.0f}; // position of camera when look_dist is zero,
-                          // or position of look_at when look_dist is greater
-                          // than zero
-  float yaw{0.0f};
-  float pitch{0.0f};
-  float look_dist{0.0f}; // zero gives fps free look,
-                         // otherwise orbit about look_at
+  as::vec3 look_at{0.0_r}; // position of camera when look_dist is zero,
+                           // or position of look_at when look_dist is greater
+                           // than zero
+  as::real yaw{0.0_r};
+  as::real pitch{0.0_r};
+  as::real look_dist{0.0_r}; // zero gives fps free look,
+                             // otherwise orbit about look_at
 
   as::affine view() const; // camera view to pass to shader
   as::affine transform() const; // camera transform to use in app
@@ -29,7 +31,7 @@ inline as::affine Camera::transform() const
   return as::affine_mul(
     as::affine_mul(
       as::affine(as::vec3::axis_z(look_dist)),
-      as::affine(as::mat3_rotation_zxy(pitch, yaw, 0.0f))),
+      as::affine(as::mat3_rotation_zxy(pitch, yaw, 0.0_r))),
     as::affine(look_at));
 }
 
